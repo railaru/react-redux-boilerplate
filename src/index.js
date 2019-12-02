c4ea3c500040bc25ac072fd6b73c3d4924b3521a
Rutenis Raila <rutenisraila@hotmail.com>
2019-12-02 14:15:45 +0000

    app/index.js: basic documetation

--- src/index.js
+++ src/index.js
@@ -3,6 +3,42 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+
+// //ACTION -> DESCRIBES WHAT YOU WANT TO DO (e.g. increment)
+// const increment = () => {
+//   return {
+//     type: 'INCREMENT'
+//   };
+// };
+// const decrement = () => {
+//   return {
+//     type: 'DECREMENT'
+//   };
+// };
+
+// //REDUCER -> DESCRIBES HOW ACTIONS TRANSFORM CURRENT STATE TO THE NEW STATE
+// const counter = (state = 0, action) => {
+//   switch (action.type) {
+//     case 'INCREMENT':
+//       return state + 1;
+
+//     case 'DECREMENT':
+//       return state - 1;
+//   }
+// };
+
+// //STORE -> GLOBALIZE STATE
+// let store = createStore(counter);
+
+// //display result:
+// store.subscribe(() => console.log(store.getState()));
+
+// //DISPATCH -> EXECUTE THE ACTION
+// store.dispatch(increment());
+// store.dispatch(increment());
+// store.dispatch(decrement());
+// store.dispatch(increment());
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
